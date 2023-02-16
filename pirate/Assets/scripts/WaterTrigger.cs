d16e1967e1558873dfc924b7df5a32ce050a8b62
cooljam206 <4017157@g.highlineschools.org>
2023-02-16 09:02:29 -0800

    platform maker added and rand offset

--- pirate/Assets/scripts/WaterTrigger.cs
+++ pirate/Assets/scripts/WaterTrigger.cs
@@ -8,7 +8,11 @@ public class WaterTrigger : MonoBehaviour
     // Start is called before the first frame update
     private void Awake()
     {
-        
+        rigid = gameObject.GetComponent<Rigidbody2D>();
+    }
+    private void Start()
+    {
+        rigid.velocity = new Vector2(0.0f, 1.0f);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
