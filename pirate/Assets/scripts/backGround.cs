0dbfb1b327984bc179a7168ee1f0ed7362e2834b
cooljam206 <4017157@g.highlineschools.org>
2023-02-24 07:52:33 -0800

    background sky added w/ script update

--- pirate/Assets/scripts/backGround.cs
+++ pirate/Assets/scripts/backGround.cs
@@ -6,10 +6,12 @@ public class backGround : MonoBehaviour
 {
     //need a bool for moving left/right
     //need a way to hook up to players.y and a 
+    public bool usesPlayerY = true;
     public bool movesHorizontly = false;
     public bool LeftOn_RightOff = false;
     public float horzSpeed = 1.0f;
     public float scale = 1;
+    public float initialOffet = 0;
     Renderer render;
     public GameObject player;//get player object so that i can access the player script
     PlayerControl playVeloc;
@@ -21,8 +23,17 @@ public class backGround : MonoBehaviour
     void Update()
     {
         playVeloc = player.GetComponent<PlayerControl>();//update player.y
-        Vector2 temp = new Vector2();
-        temp.y = scale * -playVeloc.getPlayerY();
+        Vector2 temp = new Vector2();//temp vector for multiple axis movemnet
+        //vertical movement
+        if (usesPlayerY)
+        {
+            temp.y = scale * -playVeloc.getPlayerY() + initialOffet;
+        }
+        else
+        {
+            temp.y = Time.time * scale + initialOffet;
+        }
+        //horizontal movement
         if (movesHorizontly)
         {
             if (LeftOn_RightOff)
